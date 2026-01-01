c685845604712fc1fad0d4edc699ac2d7bc8cbd7
varnikaBharalia <vbharalia11@gmail.com>
2026-01-01 19:01:10 +0530

    CPT solving placeholder issue

--- chatPromptemplate_6.py
+++ chatPromptemplate_6.py
@@ -1,9 +1,18 @@
+# DYNAMIC MULTI CHATS PROMPTING TEMPLATING USING ChatPromptTemplate
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import SystemMessage , HumanMessage
 
 chat_template = ChatPromptTemplate([
-    SystemMessage(content="You are a helpful {domain} expert."),
-    HumanMessage(content="Explain in simple terms , what is {topic} ?")
+    # To avoid the issue of placeholders not being replaced, we should use f-strings or format method to create the messages with actual values.
+    # We'll use here the tuple - (role , content)
+
+    ('system', "You are a helpful {domain} expert."),
+    ('human', "Explain in simple terms , what is {topic} ?")
+
+
+    # SystemMessage(content="You are a helpful {domain} expert."),
+    # HumanMessage(content="Explain in simple terms , what is {topic} ?")
 ])
 
 prompt = chat_template.invoke({
